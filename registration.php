6b80bd8a444be226ff2e1f71b9f368d53b4dcf23
audrey-roch houssou <audreyroch.houssou@gmail.com>
2020-09-04 10:32:49 +0200

    registration module and di.xml

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Experius_DonationProduct',
+    'Fidesio_Donation',
     __DIR__
 );
